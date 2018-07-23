e96355fe25ccae67c119ea185aa91152d929afbb
alessandro bologna <alessandro_bologna@timeinc.com>
2018-07-23 16:54:43 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # parameter-store-replicator
 AWS System Manager Parameter Store cross region replicator
+
+### What is this
+This is a sample Lambda to replicate the variables stored in [AWS System Manager Parameter Store](https://docs.aws.amazon.com/systems-manager/latest/userguide/systems-manager-paramstore.html) across different regions, in the same account. 
+It listes to CloudWatch events, and when a parameter is updated, it tries to replicate it to another target region as defined during deployment of the Lambda function. 
+It supports two way replication, so you can deploy the same lambda in, say `us-east-1` and `us-west-2` specifying the other region as a replication target.
+
+### Installing it
+The project depends on the [serverless framework](https://serverless.com/), so you will want to install that first:
+
+```bash
+$ npm install -g serverless
+```
+
+After you have it installed, just deploy your function to the main region, specifying as target the replication region. For instance, to replicate from `us-east-1` to `us-west-2`:
+
+```bash
+$ sls deploy --region us-east-1  --target=us-west-2
+```
+
+You can establish two way replication by deploying the same lambda to the target region:
+
+```bash
+$ sls deploy --region us-west-2  --target=us-east-1
+```
+
+If you are using two way replication, there's logic in place to avoid the "ping pong" effect, where a region replicating to another causes the replication to happen in the other direction. Currenlty, the logic is just based on value and type for a given parameter. If they already exist in the target region, and their value match, replication is not performed.
+
+### Testing it
+Just deploy the code, as described above, and run 
+```bash
+$ aws ssm put-parameter --name "/root/test4" --value "test6" --type "String" --overwrite --region us-east-1
+```
+
+After a few seconds, you should be able to do (results included):
+```bash
+$ aws ssm get-parameter --name "/root/test4" --region us-east-1
+{
+    "Parameter": {
+        "Version": 65, 
+        "Type": "String", 
+        "Name": "/root/test4", 
+        "Value": "test6"
+    }
+}
+
+$ aws ssm get-parameter --name "/root/test4" --region us-west-2
+{
+    "Parameter": {
+        "Version": 14, 
+        "Type": "String", 
+        "Name": "/root/test4", 
+        "Value": "test6"
+    }
+```
---
+++ handler.js
@@ -0,0 +1,85 @@
+const AWSXRay = require('aws-xray-sdk-core')
+const AWS = AWSXRay.captureAWS(require('aws-sdk'))
+
+const sourceSSM = new AWS.SSM({
+  region: process.env.AWS_DEFAULT_REGION
+})
+const targetSSM = new AWS.SSM({
+  region: process.env.AWS_TARGET_REGION
+})
+
+const checkTarget = async (event) => {
+  try {
+    // check if target exists already
+    return await targetSSM.getParameter({
+      Name: event.detail.name,
+      WithDecryption: true
+    }).promise()
+  } catch (error) {
+    // we will consider a ParameterNotFound response from the target a non error
+    if (error.code !== 'ParameterNotFound') {
+      return Promise.reject(error)
+    }
+    return Promise.resolve()
+  }
+}
+
+const update = async (event) => {
+  // get the source value
+  const sourceParam = await sourceSSM.getParameter({
+    Name: event.detail.name,
+    WithDecryption: true
+  }).promise()
+
+  const targetParam = await checkTarget(event)
+  if (!targetParam || targetParam.Parameter.Value !== sourceParam.Parameter.Value || targetParam.Parameter.Type !== sourceParam.Parameter.Type) {
+    // remove the version
+    delete sourceParam.Parameter.Version
+    // enable overwrites
+    sourceParam.Parameter.Overwrite = true
+    return targetSSM.putParameter(sourceParam.Parameter).promise()
+  } else {
+    console.log(`Parameter ${event.detail.name} is already in ${process.env.AWS_TARGET_REGION} with the same value and type, ignoring`)
+    return Promise.resolve()
+  }
+}
+
+const remove = async (event) => {
+  try {
+    return targetSSM.deleteParameter({
+      Name: event.detail.name
+    }).promise()
+  } catch (error) {
+    if (error.code === 'ParameterNotFound') {
+      console.log(`Parameter ${event.detail.name} was not found in ${process.env.AWS_TARGET_REGION}, ignoring`)
+      return Promise.resolve()
+    }
+    return Promise.reject(error)
+  }
+}
+
+const operations = {
+  Create: update,
+  Update: update,
+  Delete: remove
+}
+
+exports.replicate = async (event, context, callback) => {
+  console.log(JSON.stringify(event))
+  try {
+    if (event.detail.operation in operations) {
+      const success = await operations[event.detail.operation](event)
+      if (success) {
+        console.log(`${event.detail.operation} result:\n${JSON.stringify(success)}`)
+      }
+    } else {
+      console.log(`Unknown operation "${event.detail.operation}":\n ${JSON.stringify(event)}`)
+    }
+  } catch (error) {
+    console.log(`Operation failed for\n ${JSON.stringify(event)}\n${JSON.stringify(error)}`)
+    if (error.retryable) {
+      return callback(error)
+    }
+  }
+  return callback(null, 'OK')
+}
