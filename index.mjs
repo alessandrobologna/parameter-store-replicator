7357890b50938f671be2552aa4041e54f0d5701d
danny-sevaro <98357929+danny-sevaro@users.noreply.github.com>
2023-06-21 11:25:21 -0400

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,86 +1,95 @@
-const AWS = require('aws-sdk');
+import { SSMClient, GetParameterCommand, PutParameterCommand, DeleteParameterCommand, GetParameterHistoryCommand } from "@aws-sdk/client-ssm";
 
-const sourceSSM = new AWS.SSM({
-  region: process.env.AWS_DEFAULT_REGION
+const sourceSSM = new SSMClient({
+    region: process.env.AWS_DEFAULT_REGION
 });
-const targetSSM = new AWS.SSM({
-  region: process.env.AWS_TARGET_REGION
+const targetSSM = new SSMClient({
+    region: process.env.AWS_TARGET_REGION
 });
 
 const checkTarget = async (event) => {
-  try {
-    // check if target exists already
-    return await targetSSM.getParameter({
-      Name: event.detail.name,
-      WithDecryption: event.detail.type == "SecureString"
-    }).promise();
-  } catch (error) {
-    // we will consider a ParameterNotFound response from the target a non error
-    if (error.code !== 'ParameterNotFound') {
-      return Promise.reject(error);
+    try {
+        // check if target exists already
+        const command = new GetParameterCommand({ Name: event.detail.name, WithDecryption: event.detail.type == "SecureString" });
+        const targetParam = await targetSSM.send(command);
+        return targetParam;
+    } catch (error) {
+        // we will consider a ParameterNotFound response from the target a non error
+        if (error.__type == 'ParameterNotFound') {
+            return null;
+        } else {
+            throw error;
+        }
     }
-    return Promise.resolve();
-  }
 };
 
 const update = async (event) => {
-  // get the source value
-  const sourceParam = await sourceSSM.getParameter({
-    Name: event.detail.name,
-    WithDecryption: event.detail.type == "SecureString"
-  }).promise();
-
-  const targetParam = await checkTarget(event);
-  if (!targetParam || targetParam.Parameter.Value !== sourceParam.Parameter.Value || targetParam.Parameter.Type !== sourceParam.Parameter.Type) {
-    // remove the unused params
-    delete sourceParam.Parameter.Version;
-    delete sourceParam.Parameter.LastModifiedDate;
-    delete sourceParam.Parameter.ARN;
-    // enable overwrites
-    sourceParam.Parameter.Overwrite = true;
-    return targetSSM.putParameter(sourceParam.Parameter).promise();
-  } else {
-    console.log(`Parameter ${event.detail.name} is already in ${process.env.AWS_TARGET_REGION} with the same value and type, ignoring`);
-    return Promise.resolve();
-  }
+    const command = new GetParameterHistoryCommand({ Name: event.detail.name, WithDecryption: event.detail.type == "SecureString" });
+    const sourceParamList = await sourceSSM.send(command);
+    const sourceParam = sourceParamList.Parameters.at(-1);
+    const targetParam = await checkTarget(event);
+    if (!targetParam || targetParam.Parameter.Value !== sourceParam.Value || targetParam.Parameter.Type !== sourceParam.Type) {
+        const command = new PutParameterCommand({
+            Overwrite: true,
+            Name: sourceParam.Name,
+            Value: sourceParam.Value,
+            Tier: sourceParam.Tier,
+            Type: sourceParam.Type
+        });
+        const response = await targetSSM.send(command);
+        return response;
+    } else {
+        console.log(`Parameter ${event.detail.name} is already in ${process.env.AWS_TARGET_REGION} with the same value and type, ignoring`);
+        return null;
+    }
 };
 
+
 const remove = async (event) => {
-  try {
-    return targetSSM.deleteParameter({
-      Name: event.detail.name
-    }).promise();
-  } catch (error) {
-    if (error.code === 'ParameterNotFound') {
-      console.log(`Parameter ${event.detail.name} was not found in ${process.env.AWS_TARGET_REGION}, ignoring`);
-      return Promise.resolve();
+    try {
+        const command = new DeleteParameterCommand({ Name: event.detail.name });
+        const response = await targetSSM.send(command);
+        return response;
+    } catch (error) {
+        // we will consider a ParameterNotFound response from the target a non error
+        if (error.__type == 'ParameterNotFound') {
+            return null;
+        } else {
+            throw error;
+        }
     }
-    return Promise.reject(error);
-  }
 };
 
 const operations = {
-  Create: update,
-  Update: update,
-  Delete: remove
+    Create: 'update',
+    Update: 'update',
+    Delete: 'remove'
 };
 
-exports.handler = async (event, context, callback) => {
-  console.log(JSON.stringify(event));
-  try {
-    if (event.detail.operation in operations) {
-      const success = await operations[event.detail.operation](event);
-      if (success) {
-        console.log(`${event.detail.operation} result:\n${JSON.stringify(success)}`);
-      }
-    } else {
-      console.log(`Unknown operation "${event.detail.operation}":\n ${JSON.stringify(event)}`);
+export const handler = async (event, context, callback) => {
+    console.log(JSON.stringify(event));
+    try {
+        if (event.detail.operation in operations) {
+            let success;
+            switch (event.detail.operation) {
+                case "Create":
+                case "Update":
+                    success = await update(event);
+                    break;
+                case "Delete":
+                    success = await remove(event);
+            }
+            if (success) {
+                console.log(`${event.detail.operation} result:\n${JSON.stringify(success)}`);
+            }
+        } else {
+            console.log(`Unknown operation "${event.detail.operation}":\n ${JSON.stringify(event)}`);
+        }
+    } catch (error) {
+        console.log(`Operation failed for\n ${JSON.stringify(event)}\n${JSON.stringify(error)}`);
+        if (error.retryable) {
+            return callback(error);
+        }
     }
-  } catch (error) {
-    console.log(`Operation failed for\n ${JSON.stringify(event)}\n${JSON.stringify(error)}`);
-    if (error.retryable) {
-      return callback(error);
-    }
-  }
-  return callback(null, 'OK');
+    return callback(null, 'OK');
 };
